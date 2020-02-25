1076a7a157c18fb3a7c48d3c28d6d4c49b41e3b5
Michael Eisel <michael.eisel@gmail.com>
2020-02-25 12:19:48 -0500

    commit

--- Formula/zld.rb
+++ Formula/zld.rb
@@ -3,7 +3,7 @@ class Zld < Formula
   desc "A faster version of ld, Apple's linker"
   url "https://github.com/michaeleisel/zld/releases/download/#{version}/zld.zip"
   sha256 "61ae5858d9a9c0c807c9007af5a70da3ae38c686a22a0ad5f6381c4c6c35314a"
-  depends_on :parallelstl
+  depends_on "parallelstl"
   homepage "https://github.com/michaeleisel/zld"
 
   def install
