66cde0f91c0c1dc84b97752084145eb14c77f317
Michael Eisel <michael.eisel@gmail.com>
2020-02-26 18:25:28 -0500

    0.1.2

--- Formula/zld.rb
+++ Formula/zld.rb
@@ -1,8 +1,8 @@
 class Zld < Formula
-  version "0.1.1"
+  version "0.1.2"
   desc "A faster version of ld, Apple's linker"
   url "https://github.com/michaeleisel/zld/releases/download/#{version}/zld.zip"
-  sha256 "6855e1269db59d1d2e49294c1792efca730b9e805f0c4b8027f50223024c8aee"
+  sha256 "bf5ca8a9cf01b18a09a7eac250a6bbc4109f7eacedde9d25c123b88d3ceaf51d"
   depends_on "parallelstl"
   homepage "https://github.com/michaeleisel/zld"
 
