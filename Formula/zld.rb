4d40c1bd12943ab3446afffff38118e40c58abbc
Michael Eisel <michael.eisel@gmail.com>
2020-03-20 11:24:23 -0400

    fix sha

--- Formula/zld.rb
+++ Formula/zld.rb
@@ -2,7 +2,7 @@ class Zld < Formula
   version "1.0.0"
   desc "A faster version of ld, Apple's linker"
   url "https://github.com/michaeleisel/zld/releases/download/#{version}/zld.zip"
-  sha256 "bf5ca8a9cf01b18a09a7eac250a6bbc4109f7eacedde9d25c123b88d3ceaf51d"
+  sha256 "f81cd618ed6ad8df733f1dd342dfe45284bcece3dd7aa5119f058bc79df970a8"
   homepage "https://github.com/michaeleisel/zld"
 
   def install
