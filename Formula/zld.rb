bb33f0e4c34d3ed78e8486591ebb6871f9e8c900
Michael Eisel <michael.eisel@gmail.com>
2021-06-25 14:26:08 -0400

    commit

--- Formula/zld.rb
+++ Formula/zld.rb
@@ -1,9 +1,9 @@
 class Zld < Formula
-  desc "A faster version of ld, Apple's linker"
-  version "1.3.1.1" # Don't update without moving off temp branch
+  desc "Faster version of ld, Apple's linker"
   homepage "https://github.com/michaeleisel/zld"
   # This is a temporary branch for debugging
   url "https://github.com/michaeleisel/zld/archive/refs/heads/homebrew-building.tar.gz"
+  version "1.3.1.1" # Don't update without moving off temp branch
   sha256 "cf74f38ad6c40024d95347b6f4f4c22e12c1c50315ba325650f8e81243728352"
   depends_on "cmake" => :build
 
