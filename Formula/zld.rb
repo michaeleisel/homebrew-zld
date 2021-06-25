d6e5638b10de7da79fac25188f9721f2a2109823
Michael Eisel <michael.eisel@gmail.com>
2021-06-25 10:50:24 -0400

    commit

--- Formula/zld.rb
+++ Formula/zld.rb
@@ -1,15 +1,15 @@
 class Zld < Formula
-  version "1.3.1" # Don't update without moving off temp branch
+  version "1.3.1.1" # Don't update without moving off temp branch
   desc "A faster version of ld, Apple's linker"
   # This is a temporary branch for debugging
-  url "https://github.com/michaeleisel/zld/archive/refs/heads/homebrew-fixes.zip"
-  sha256 "b52cbc4e867f0dad25add6a96cba028958866d27c752491e319fb427c97e6dea"
+  url "https://github.com/michaeleisel/zld/archive/refs/heads/homebrew-building.zip"
+  sha256 "58fe5e803d82e4b37301fd0789cec5bd83a3bf8487d7812c02511b07325a497d"
   homepage "https://github.com/michaeleisel/zld"
   depends_on "cmake" => :build
 
   def install
     system "make", "clean"
-    system "make", "build_homebrew"
+    system "make", "build"
     bin.install "build/Build/Products/Release/zld"
   end
 end
