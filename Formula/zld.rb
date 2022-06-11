50a051c3f624ea292ae202d44fc3ab69a8ac12ca
Michael Eisel <michael.eisel@gmail.com>
2022-06-11 11:27:54 -0400

    Master

--- Formula/zld.rb
+++ Formula/zld.rb
@@ -1,8 +1,8 @@
 class Zld < Formula
-  version "1.3.3"
+  version "1.3.3.1"
   desc "A faster version of ld, Apple's linker"
-  url "https://github.com/michaeleisel/zld/archive/refs/heads/homebrew-fixes-1.3.3.zip"
-  sha256 "76fc45c1feb5e77466d3fb1e4f242abc95ef293b93304a5e1706fd5758c54a17"
+  url "https://github.com/michaeleisel/zld/archive/refs/tags/1.3.4.zip"
+  sha256 "4a29ef7433ae5b7b45dfdba80f599026bac760ccf59a68fa7bcff696dfc3993e"
   homepage "https://github.com/michaeleisel/zld"
   depends_on "cmake" => :build
 
