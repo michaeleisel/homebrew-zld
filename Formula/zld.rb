84a4e5f85be26e87bdeb2a3349379e6a0d19ef36
Michael Eisel <michael.eisel@gmail.com>
2022-06-11 12:22:38 -0400

    Master

--- Formula/zld.rb
+++ Formula/zld.rb
@@ -1,5 +1,5 @@
 class Zld < Formula
-  version "1.3.3.2"
+  version "1.3.4"
   desc "A faster version of ld, Apple's linker"
   url "https://github.com/michaeleisel/zld/archive/709187361b9871984eaf92057c74cc4aa4a4906f.zip"
   sha256 "1803565f6bf642ba21381798cf1aa50c34eaca28ba4c5c3d7e38a1816f535935"
