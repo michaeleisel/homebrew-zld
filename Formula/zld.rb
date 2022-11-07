49df886a77199e12ac825cab952fdedd805148ce
Michael Eisel <michael.eisel@gmail.com>
2022-11-07 06:33:43 -0500

    1.3.9

--- Formula/zld.rb
+++ Formula/zld.rb
@@ -1,8 +1,8 @@
 class Zld < Formula
-  version "1.3.8"
+  version "1.3.9"
   desc "A faster version of ld, Apple's linker"
-  url "https://github.com/michaeleisel/zld/archive/54f29464a6e6926f7535f3a990c6aca3dfc4f513.zip"
-  sha256 "0b26bc1330db1f9d9470b15a6f5fb94f06cef7f70f4bf8ccb07ac3c72274cfdc"
+  url "https://github.com/michaeleisel/zld/archive/refs/tags/1.3.9.zip"
+  sha256 "c65a54b48078303dd60b0e920f0ee165b03f86129465beacbdadf8724bac6b6a"
   homepage "https://github.com/michaeleisel/zld"
   depends_on "cmake" => :build
 
